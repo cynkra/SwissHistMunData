71181e408f9ccbd60a9feed605d3f8204adffb56
Thomas Knecht <t.knecht@hotmail.com>
2019-10-14 21:02:18 +0200

    Search for bug

--- tic.R
+++ tic.R
@@ -1,10 +1,10 @@
 do_package_checks()
 
 if (ci_has_env("id_rsa") && !ci_is_tag()) {
-  get_stage("install") %>%
-    add_step(step_install_cran("desc")) %>%
-    add_step(step_install_cran("dplyr")) %>%
-    add_step(step_install_cran("pkgload"))
+  # get_stage("install") %>%
+  #   add_step(step_install_cran("desc")) %>%
+  #   add_step(step_install_cran("dplyr")) %>%
+  #   add_step(step_install_cran("pkgload"))
 
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
